24f711a038872c248cea236dde55585227294cd6
Zeryther <zeryther@gmail.com>
2017-09-11 11:12:37 +0200

    Initial commit

---
+++ src/main/java/net/wrathofdungeons/dungeonhub/DungeonHub.java
@@ -0,0 +1,10 @@
+package net.wrathofdungeons.dungeonhub;
+
+import net.wrathofdungeons.dungeonapi.DungeonAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DungeonHub extends JavaPlugin {
+    public void onEnable(){
+
+    }
+}
