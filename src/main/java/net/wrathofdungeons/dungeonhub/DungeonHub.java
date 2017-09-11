966e23240ce1db0d5f469473e92e253a875ad896
Zeryther <zeryther@gmail.com>
2017-09-11 12:37:46 +0200

    Disabled weather

--- src/main/java/net/wrathofdungeons/dungeonhub/DungeonHub.java
+++ src/main/java/net/wrathofdungeons/dungeonhub/DungeonHub.java
@@ -3,7 +3,9 @@ package net.wrathofdungeons.dungeonhub;
 import net.wrathofdungeons.dungeonapi.DungeonAPI;
 import net.wrathofdungeons.dungeonhub.listener.PlayerChatListener;
 import net.wrathofdungeons.dungeonhub.listener.PlayerJoinListener;
+import net.wrathofdungeons.dungeonhub.listener.WeatherChangeListener;
 import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,8 +14,16 @@ public class DungeonHub extends JavaPlugin {
 
     public void onEnable(){
         mainWorld = Bukkit.getWorlds().get(0);
+        mainWorld.setGameRuleValue("doMobSpawning","false");
+        mainWorld.setGameRuleValue("doFireTick","false");
+        mainWorld.setGameRuleValue("doDaylightCycle","false");
+        mainWorld.setTime(6000);
+        mainWorld.setStorm(false);
+        mainWorld.setThundering(false);
+        mainWorld.setDifficulty(Difficulty.PEACEFUL);
 
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(),this);
         Bukkit.getPluginManager().registerEvents(new PlayerChatListener(),this);
+        Bukkit.getPluginManager().registerEvents(new WeatherChangeListener(),this);
     }
 }
